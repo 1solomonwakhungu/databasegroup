ea6afc4f245de3e4354ad5051e1b88293301d7c5
Ved Nigam <ved.nigam@icloud.com>
2023-04-19 12:23:26 -0500

    adding action to button

--- doctor_functions/doctor.py
+++ doctor_functions/doctor.py
@@ -3,5 +3,6 @@ from connector import MYDB
 DOCTOR_FUNCTIONS = {
     1: "assign prescription",
     2: "get patient information",
-    3: "go to room"
+    3: "go to room",
+    4: "example function"
 }

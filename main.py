d14253d88d4f7d1bfdd6e9895cf71b4236ac33db
Ved Nigam <ved.nigam@icloud.com>
2023-04-19 10:55:58 -0500

    commited with numpy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, redirect, url_for, session
 import mysql.connector
+import numpy
 
 # Connect to local sql server
 mydb = mysql.connector.connect(host = "localhost", user = "root", passwd = "VedNigam1", database = "hospital_db")

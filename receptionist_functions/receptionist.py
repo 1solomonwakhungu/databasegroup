07c1e2504f409dfa09a0a25ba9dfe152349c1fdf
ASharif23 <113036152+ASharif23@users.noreply.github.com>
2023-04-23 14:23:38 -0500

    Update receptionist.py

--- receptionist_functions/receptionist.py
+++ receptionist_functions/receptionist.py
@@ -16,16 +16,16 @@ def assign_nurse_room(nurse_id, room_number):
     mycursor.execute('SELECT * FROM room WHERE room_number = %s', (room_number,))
     room = mycursor.fetchone()
     if room is None:
-        return "Invalid room number"
+        return {}
 
     mycursor.execute('SELECT * FROM room_assignment WHERE room_number = %s AND checkout_date IS NULL', (room_number,))
     current_assignment = mycursor.fetchone()
     if current_assignment is not None:
-        return "Room is not available"
+        return {}
 
     mycursor.execute('INSERT INTO room_assignment (room_number, nurse_id, checkin_date) VALUES (%s, %s, NOW())', (room_number, nurse_id))
     MYDB.commit()
-    return "Nurse assigned to room successfully"
+    return nurses
 
 # function by Solomon
 def view_doctors():

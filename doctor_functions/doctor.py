5fffaf7ed98ea2635a1461bec07e7d8760da796f
Rob108 <shoopdawhoop24@gmail.com>
2023-04-24 16:21:44 -0500

    adad

--- doctor_functions/doctor.py
+++ doctor_functions/doctor.py
@@ -22,15 +22,13 @@ def perscription(reportID, medicineName, value):
             # Execute SQL Statement to add/update/remove new persciption and reportID
             mycursor.execute('INSERT INTO medicine (medicineId, reportId, medicineName) VALUES (%s, %s, %s)',(medicineID, reportID, medicineName))
             connector.MYDB.commit()
-            output = "Successfully added perscription to patient report ID: %s", (reportID)
-            return (output)
+            return "Successfully added perscription medicine"
         # IF remove perscription
     if value == 'Neagive':
             # Execute SQL statement to remove perscription from list
             mycursor.execute('DELETE FROM medicine WHERE medicineId = %s AND reportId = %s AND medicineName = %s',(medicineID, reportID, medicineName))
             connector.MYDB.commit()
-            ouput = "Successfully removed medicine from patient reportID: %s", (reportID)
-            return (output)
+            return "Successfully removed perscription from medicine"
         
 
 # function by Ved

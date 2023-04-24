9f4ffb271a6cceb7546ee5f1c60842941144fe6e
Solomon Wakhungu <1solomonwakhungu@gmail.com>
2023-04-24 00:24:38 -0500

    remove the instruct

--- receptionist_functions/receptionist.py
+++ receptionist_functions/receptionist.py
@@ -6,28 +6,35 @@ RECEPTIONIST_FUNCTIONS = {
 }
 
 # function by Alaan
+
+
 def assign_nurse_room(nurse_id, room_number):
     mycursor = MYDB.cursor()
-    mycursor.execute('SELECT * FROM employee WHERE id = %s AND role = "nurse"', (nurse_id,))
+    mycursor.execute('SELECT * FROM employee WHERE essn = %s', (nurse_id,))
     nurse = mycursor.fetchone()
     if nurse is None:
         return {}
 
-    mycursor.execute('SELECT * FROM room WHERE room_number = %s', (room_number,))
+    mycursor.execute(
+        'SELECT * FROM room WHERE room_number = %s', (room_number,))
     room = mycursor.fetchone()
     if room is None:
         return {}
 
-    mycursor.execute('SELECT * FROM room_assignment WHERE room_number = %s AND checkout_date IS NULL', (room_number,))
+    mycursor.execute(
+        'SELECT * FROM room_assignment WHERE room_number = %s AND checkout_date IS NULL', (room_number,))
     current_assignment = mycursor.fetchone()
     if current_assignment is not None:
         return {}
 
-    mycursor.execute('INSERT INTO room_assignment (room_number, nurse_id, checkin_date) VALUES (%s, %s, NOW())', (room_number, nurse_id))
+    mycursor.execute(
+        'INSERT INTO room_assignment (room_number, nurse_id, checkin_date) VALUES (%s, %s, NOW())', (room_number, nurse_id))
     MYDB.commit()
     return nurses
 
 # function by Solomon
+
+
 def view_doctors():
     mycursor = connector.MYDB.cursor()
     mycursor.execute('SELECT * FROM doctorinfoview')
